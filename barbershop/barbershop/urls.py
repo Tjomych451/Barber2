33efef9cf61d5fe1cb3b4753a6dbf58b6399fe67
Tjomych451 <art115665@rambler.ru>
2025-03-12 20:41:18 +0300

    Add files via upload
    обновление

---
+++ barbershop/barbershop/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from appointments import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('thank-you/', views.thank_you, name='thank_you'),
+    path('get-services/<int:master_id>/', views.get_services, name='get_services'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
